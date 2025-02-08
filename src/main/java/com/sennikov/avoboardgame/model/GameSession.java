07c0814adbf7e2605b624ed6f0489764dec9952b
Anatolii Sennikov <anatoliy.sennikov@avotech.uz>
2025-02-08 21:25:39 +0500

    change

--- src/main/java/com/sennikov/avoboardgame/model/GameSession.java
+++ src/main/java/com/sennikov/avoboardgame/model/GameSession.java
@@ -4,10 +4,8 @@ import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.hibernate.annotations.CreationTimestamp;
 
 import java.time.LocalDate;
-import java.time.LocalDateTime;
 import java.util.List;
 
 @Entity
