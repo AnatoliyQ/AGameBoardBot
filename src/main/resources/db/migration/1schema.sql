3aed37f47acca133177edb5db2705bc9cb0de434
Anatolii Sennikov <anatoliy.sennikov@avotech.uz>
2025-02-13 18:39:47 +0500

    test

--- src/main/resources/db/migration/1schema.sql
+++ src/main/resources/db/migration/1schema.sql
@@ -11,7 +11,7 @@ CREATE TABLE IF NOT EXISTS board_games (
     name VARCHAR(255) NOT NULL UNIQUE,
     type GAME_TYPE NOT NULL,
     players VARCHAR(255),
-    owner VARCHAR(255),
+    owner VARCHAR(255) REFERENCES app_users(telegram_username) DEFERRABLE INITIALLY DEFERRED,
     image_url TEXT,
     description TEXT
 );
@@ -26,13 +26,12 @@ CREATE TABLE IF NOT EXISTS app_users (
 
 CREATE TABLE IF NOT EXISTS game_sessions (
     id SERIAL PRIMARY KEY,
-    game_id BIGINT NOT NULL,
-    play_date DATE NOT NULL,
-    FOREIGN KEY (game_id) REFERENCES board_games(id)
+    game_id BIGINT NOT NULL REFERENCES board_games(id) DEFERRABLE INITIALLY DEFERRED,
+    play_date DATE NOT NULL
 );
 
 CREATE TABLE game_session_players (
-    session_id INTEGER REFERENCES game_sessions(id),
+    session_id INTEGER REFERENCES game_sessions(id) DEFERRABLE INITIALLY DEFERRED,
     player_username VARCHAR(32) REFERENCES app_users(telegram_username) DEFERRABLE INITIALLY DEFERRED,
     is_winner BOOLEAN NOT NULL,
     PRIMARY KEY (session_id, player_username)
