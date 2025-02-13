16ba68b1b3223191767bab5365448909da9504f4
Anatolii Sennikov <anatoliy.sennikov@avotech.uz>
2025-02-13 18:14:49 +0500

    test

--- src/main/resources/db/migration/2data.sql
+++ src/main/resources/db/migration/2data.sql
@@ -26,18 +26,18 @@ VALUES
 INSERT INTO app_users (first_name, last_name, telegram_username, is_employee)
 VALUES
     ('Анатолий', 'Сенников', 'AnatolSn', true),
+    ('Оксана', 'Шеина', 'i_am_super', true),
+    ('Наталья', 'Перминова', 'nata_dubl_bubl', true),
+    ('Руслан', 'Юн', 'riv1sky', true),
+    ('Шахзод', 'Алибоев', 'DDI_C', true),
+    ('Дмитрий', 'Виноградов', 'mrDmitryVinogradov', true),
     ('Роман', 'Степин', 'romcheg06', true),
     ('Илья', 'No', 'ThisIsHoroshoo', true),
     ('Виктор', 'Медведев', 'Viktor_Med', true),
     ('Станислав', 'Рябец', 'stasryabets', true),
     ('Владислав', 'Зборовский', 'petooh_5g', true),
-    ('Руслан', 'Юн', 'riv1sky', true),
-    ('Оксана', 'Шеина', 'i_am_super', true),
-    ('Шахзод', 'Алибоев', 'DDI_C', true),
     ('Фарход', 'Данияров', 'farkhod89', true),
-    ('Дмитрий', 'Виноградов', 'mrDmitryVinogradov', true),
     ('Дмитрий', 'Степанов', 'LarsAlsir', true),
-    ('Наталья', 'Перминова', 'nata_dubl_bubl', true),
     ('Юлия', 'Виноградова', 'yulka_ka', false),
     ('Мария', 'Борисик', 'MaryOnAir90', true),
     ('Олег', 'Стрельников', 'Strelnikoff', false);
