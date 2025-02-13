e8818e95d722b3c4e273e23de8a087d3f4e2acdb
Anatolii Sennikov <anatoliy.sennikov@avotech.uz>
2025-02-14 01:00:33 +0500

    test

---
+++ src/main/resources/db/migration/2data.sql
@@ -0,0 +1,18 @@
+-- Начальные пользователи
+INSERT INTO app_users (first_name, last_name, telegram_username, is_employee) VALUES
+    ('Анатолий', 'Сенников', 'AnatolSn', true),
+    ('Оксана', 'Шеина', 'i_am_super', true),
+    ('Наталья', 'Перминова', 'nata_dubl_bubl', true),
+    ('Руслан', 'Юн', 'riv1sky', true),
+    ('Шахзод', 'Алибоев', 'DDI_C', true),
+    ('Дмитрий', 'Виноградов', 'mrDmitryVinogradov', true),
+    ('Роман', 'Степин', 'romcheg06', true),
+    ('Илья', 'No', 'ThisIsHoroshoo', true),
+    ('Виктор', 'Медведев', 'Viktor_Med', true),
+    ('Станислав', 'Рябец', 'stasryabets', true),
+    ('Владислав', 'Зборовский', 'petooh_5g', true),
+    ('Фарход', 'Данияров', 'farkhod89', true),
+    ('Дмитрий', 'Степанов', 'LarsAlsir', true),
+    ('Юлия', 'Виноградова', 'yulka_ka', false),
+    ('Мария', 'Борисик', 'MaryOnAir90', true),
+    ('Олег', 'Стрельников', 'Strelnikoff', false);
